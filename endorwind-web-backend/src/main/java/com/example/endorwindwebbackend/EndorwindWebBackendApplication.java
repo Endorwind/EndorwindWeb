1d3b6b854950bc4b40f4f07958d194751ae66933
kornelIlles <kornelIlles10@gmail.com>
2022-11-16 18:32:37 +0100

    first commit

---
+++ endorwind-web-backend/src/main/java/com/example/endorwindwebbackend/EndorwindWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.endorwindwebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndorwindWebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndorwindWebBackendApplication.class, args);
+    }
+
+}
---
+++ endorwind-web-backend/src/test/java/com/example/endorwindwebbackend/EndorwindWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.endorwindwebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndorwindWebBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
